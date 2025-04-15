8d2cc88138e8392769d16bcd8b3933569cda6070
Dmitriy <dmitriykaras06@yandex.ru>
2025-04-15 20:06:37 +0300

    The first commit!

---
+++ l1/l1/l1/func.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace l1
+{
+    class functions
+    {
+
+        //(1+|x|)/(1+x)^1/3
+        public static void simplefunction()
+        {
+            Console.Write("Введите x: ");
+            double x = double.Parse(Console.ReadLine());
+            double result = (1 + Math.Abs(x)) / Math.Pow(1 + x, 1.0 / 3.0);
+            Console.WriteLine($"Результат: {result}\n");
+        }
+
+        //+1 если число положительное, иначе вывести его же
+        public static void conditionfunction()
+        {
+            Console.Write("Введите число: ");
+            int num = int.Parse(Console.ReadLine());
+            if (num > 0)
+                num ++;
+            Console.WriteLine($"Резульат: {num}\n");
+        }
+
+        //A и B, найти сумму диапозона от числа A до B выключитльно
+        public static void loopfunction()
+        {
+            Console.Write("Введите А: ");
+            int A = int.Parse(Console.ReadLine());
+            Console.Write("Введите B(B>A): ");
+            int B = int.Parse(Console.ReadLine());
+
+            if (B <= A)
+            {
+                Console.WriteLine("Ошибка. B должен быть больше А\n");
+                return;
+            }
+
+            int sum = 0;
+            for (int i = A; i <= B; i++)
+                sum += i;
+
+            Console.WriteLine($"Резульат: {sum}\n");
+
+        }
+    }
+}   
+
---
+++ l1/l1/l1/main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using l1;
+
+class main
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Выберите функцию:\n 1. SimpleFunction\n 2. ConditionFunction \n 3. LoopFunction \n 0. Выход");
+            Console.Write("Ваш выбор: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    functions.simplefunction();
+                    break;
+                case 2:
+                    functions.conditionfunction();
+                    break;
+                case 3:
+                    functions.loopfunction();
+                    break;
+                case 0:
+                    Console.WriteLine("Выход из программы");
+                    return;
+                default:
+                    Console.WriteLine("Неправиьный выбор\n");
+                    break;
+                
+
+            }
+        }
+    }
+
+}
