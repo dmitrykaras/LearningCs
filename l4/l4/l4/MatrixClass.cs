f88e558b5170cb46502058177b1b062ca7cb5b81
Dmitriy <dmitriykaras06@yandex.ru>
2025-05-16 09:58:43 +0300

    l4?

---
+++ l4/l4/l4/MatrixClass.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+public class Matrix
+{
+    private double[,] data = new double[4, 4];
+
+    public Matrix() //конструктор по умолчанию с нулями
+    {
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                data[i, j] = 0.0;
+    }
+
+    public Matrix(double[,] values) //конструктор с параметрами 
+    {
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                data[i, j] = values[i, j];
+    }
+
+    public Matrix(Matrix other) //конструтор копирования
+    {
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                data[i, j] = other.data[i, j];
+    }
+
+    ~Matrix() //деструктор
+    {
+        Console.WriteLine("Деструктор вызван");
+    }
+
+    public void SetElement(int row, int col, double value) //метод установки значения элемента
+    {
+        data[row, col] = value;
+    }
+
+    public double GetElement(int row, int col) //метод получения значения элемента
+    {
+        return data[row, col];
+    }
+
+    public void MultiplyByScalar(double scalar) //метод умножения матрицы на число
+    {
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                data[i, j] *= scalar;
+    }
+
+    public Matrix Transpose() //метод транспонирование
+    {
+        Matrix result = new Matrix();
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                result.data[i, j] = data[j, i];
+        return result;
+    }
+
+    public static Matrix operator +(Matrix a, Matrix b) //перегрузка оператора +
+    {
+        Matrix result = new Matrix();
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                result.data[i, j] = a.data[i, j] + b.data[i, j];
+        return result;
+    }
+
+    public static Matrix operator -(Matrix a, Matrix b) //перегрузка оператора -
+    {
+        Matrix result = new Matrix();
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                result.data[i, j] = a.data[i, j] - b.data[i, j];
+        return result;
+    }
+
+    public static Matrix ReadMatrix() //
+    {
+        double[,] values = new double[4, 4];
+        Console.WriteLine("Введите элементы матрицы 4x4:");
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                values[i, j] = Convert.ToDouble(Console.ReadLine());
+        return new Matrix(values);
+    }
+
+    public void Print() //метод для вывода матрицы
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+                Console.Write($"{data[i, j],8:F2} ");
+            Console.WriteLine();
+        }
+    }
+
+}
---
+++ l4/l4/l4/main.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+class main
+{
+    static void Main()
+    {
+        Matrix m1 = new Matrix(); // по умолчанию
+        Matrix m2 = new Matrix(new double[,] {
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12},
+            {13,14,15,16}
+        });
+
+        Matrix m3 = new Matrix(m2); // копирование
+        Matrix m4 = null; // через указатель
+
+        while (true)
+        {
+            Console.WriteLine("Меню: ");
+            Console.WriteLine("1. Создание и вывод состояния объекта авторского класса");
+            Console.WriteLine("2. Изменение значения члена-данного");
+            Console.WriteLine("3. Применение уникального метода класса");
+            Console.WriteLine("4. Применение арифметических и операторов отношений к объектам класса");
+            Console.WriteLine("5. Ввод матрицы 4х4");
+            Console.WriteLine("0. Выход");
+            Console.Write("Ваш выбор: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch(choice)
+            {
+                case 1: //вызов Print
+                    Console.WriteLine("Матрица 1:");
+                    m1.Print();
+                    Console.WriteLine("Матрица 2:");
+                    m2.Print();
+                    break;
+
+                case 2: //изменение элемента по i и j
+                    Console.Write("Введите i(0-3): ");
+                    int i = int.Parse(Console.ReadLine());
+                    Console.Write("Введите j(0-3): ");
+                    int j = int.Parse(Console.ReadLine());
+                    Console.Write("Введите значение: ");
+                    double v = double.Parse(Console.ReadLine());
+                    if (i < 0 || i > 3 || j < 0 || j > 3)
+                    {
+                        Console.WriteLine("Индексы должны быть от 0 до 3.");
+                        break;
+                    }
+                    m1.SetElement(i, j, v);
+                    Console.WriteLine("Матрица после изменения:");
+                    m1.Print();
+                    break;
+
+                case 3: //транспонирование матрицы
+                    m2 = m2.Transpose();
+                    m2.Print();
+                    break;
+                  
+                case 4: //арифметические действия
+                    Console.WriteLine("Выберите действие: 1. Умножение; 2. Разность матриц; 3. Сложение матриц");
+                    Console.Write("Ваш выбор: ");
+                    int choiceObj = int.Parse(Console.ReadLine());
+                    switch (choiceObj)
+                    {
+                        case 1:
+                            Console.Write("Введите число: ");
+                            double s = double.Parse(Console.ReadLine());
+                            m2.MultiplyByScalar(s);
+                            m2.Print();
+                            break;
+                        case 2:
+                            Matrix diff = m1 - m2;
+                            Console.WriteLine("Разность:");
+                            diff.Print();
+                            break;
+                        case 3:
+                            Matrix sum = m1 + m2;
+                            Console.WriteLine("Сумма:");
+                            sum.Print();
+                            break;
+                        case 0:
+                            Console.WriteLine("Отмена выбора\n");
+                            break;
+                        default:
+                            Console.WriteLine("Неправиьный выбор\n");
+                            break;
+                    }
+                    break;
+
+                case 5:
+                    m4 = Matrix.ReadMatrix();
+                    m4.Print();
+                    break;
+                case 0:
+                    Console.WriteLine("Выход из программы");
+                    return;
+                default:
+                    Console.WriteLine("Неправильный выбор\n");
+                    break;
+            }
+        }
+    }
+}
